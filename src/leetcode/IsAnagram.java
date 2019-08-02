7d5aeb4c2d888f1ef22519b4f4d4cb63d70a8562
gsdukbh <gsdukbh@gmail.com>
2019-08-02 20:52:25 +0800

    今日份一题

---
+++ src/leetcode/IsAnagram.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class IsAnagram {
+    /**
+     *
+     */
+    public boolean isAnagram(String s, String t) {
+        int a=0;
+        if( s.length()!=t.length()){
+            return false;
+        }
+        for (int i=0;i<s.length();i++){
+
+        }
+        return false;
+    }
+}
