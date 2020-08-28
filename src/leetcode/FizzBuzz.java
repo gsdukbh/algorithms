3f8930ed4ed42b8677e7d4faef4720623f74169c
lijiawei <gsdukbh@gamil.com>
2020-08-28 18:04:44 +0800

    2020年8月28日

--- src/leetcode/FizzBuzz.java
+++ src/leetcode/FizzBuzz.java
@@ -14,22 +14,23 @@ public class FizzBuzz {
     public static List<String> fizzBuzz(int n) {
         List<String> res = new ArrayList<>();
         Map<Integer, String> map = Map.of(
-                3, "Fizz",
-                5, "Buzz"
+                5, "Buzz",
+                3, "Fizz"
+
         );
         for (int i = 1; i <= n; i++) {
-            String tem = "";
+            StringBuilder tem = new StringBuilder();
             for (Integer key :
                     map.keySet()) {
                 if (i % key == 0) {
-                    tem += map.get(key);
+                    tem.append(map.get(key));
                 }
             }
 
-            if ("".equals(tem)) {
-                tem +=Integer.toString(i) ;
+            if ("".equals(tem.toString())) {
+                tem.append(Integer.toString(i));
             }
-            res.add(tem);
+            res.add(tem.toString());
         }
         return res;
     }
